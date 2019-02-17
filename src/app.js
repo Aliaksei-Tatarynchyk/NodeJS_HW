85b6b08bc9ce5f9763354f8244b55a0c5207839a
Aliaksei Tatarynchyk <aliaksei_tatarynchyk@epam.com>
2019-02-17 23:53:49 +0300

    HW1 - Adding Modules

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 console.info("I'm running...")
+
+import config from "./config/config.json" 
+console.log(config.name);
+
+import { User, Product } from "./models/models"
+const user = new User();
+const product = new Product();
