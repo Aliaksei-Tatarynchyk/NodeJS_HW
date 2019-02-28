c8a199f52d8b3741c0e34ee585834cca2198e8ec
Aliaksei Tatarynchyk <aliaksei_tatarynchyk@epam.com>
2019-02-28 16:30:50 +0300

    HW2 - Async development
    - fix code review comments - get rid of collections, add lodash

--- src/csv/dirwatcher.js
+++ src/csv/dirwatcher.js
@@ -1,14 +1,15 @@
 import EventEmitter from 'events'
-import Set from 'collections/set'
 import fs from 'fs'
 import {promisify} from 'util'
+import difference from 'lodash/difference'
+
 const readdir = promisify(fs.readdir);
 
 export default class DirWatcher extends EventEmitter {
 
   constructor(path, delay) {
     super();
-    this.processedFiles = new Set();
+    this.processedCsvFiles = new Set();
     this.watch(path, delay);
   }
   
@@ -19,30 +20,18 @@ export default class DirWatcher extends EventEmitter {
     this.watchInterval = setInterval(() => {
       readdir(path)
         .then((filesFromFS) => {
-          const newFiles = new Set(filesFromFS).difference(this.processedFiles);
-          if (newFiles.any()) {
-            this.processedFiles.addEach(newFiles);
-            this.emit("change", newFiles.map(fileName => {return path + '/' + fileName;}));
-          }
+          const newFiles = difference(filesFromFS, [...this.processedCsvFiles]);
+          newFiles
+            .filter(file => file.endsWith('.csv'))
+            .forEach((newCsvFile) => {
+              this.processedCsvFiles.add(newCsvFile);
+              this.emit("change", `${path}/${newCsvFile}`);
+            });
         })
         .catch((error) => {
           console.log(error);
           throw new Error(error);
         });
-
-        /*
-        fs.readdir(path, (error, filesFromFS) => {
-          if (error) {
-            console.log(error);
-            throw error;
-          }
-          const newFiles = new Set(filesFromFS).difference(this.processedFiles);
-          if (newFiles.any()) {
-            this.processedFiles.addEach(newFiles);
-            this.emit("change", newFiles.map(fileName => {return path + '/' + fileName;}));
-          }
-        });
-        */
     }, delay);
   }
 }
--- src/hw2.js
+++ src/hw2.js
@@ -3,6 +3,6 @@ import Importer from "./csv/importer"
 
 console.log("---> HW2 - Async development");
 console.log("Watching for changes in 'data' directory and import CSV files as json:");
-const dirwatcher = new DirWatcher("./data", 1e+3);
+const dirwatcher = new DirWatcher("./data", 5e+3);
 const importer = new Importer(dirwatcher);
 // console.log("importSync: ", importer.importSync("./data/entertainment.csv"));
